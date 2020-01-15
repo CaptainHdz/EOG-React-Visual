173fc9c0eec968ed08ecd2cda1efbbda551a34e5
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-01-15 15:37:52 -0600

    websocket successfull, no errors

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,12 @@ import ChartCard from './components/ChartCard';
 import ChartMenu from './components/ChartMenu';
 import Chart from 'react-apexcharts';
 import { WebSocketLink } from 'apollo-link-ws';
-import { split} from 'apollo-link';
-import ApolloClient from 'apollo-boost';
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import ApolloClient from 'apollo-client';
+import {InMemoryCache} from 'apollo-boost';
 import { gql } from 'apollo-boost';
+import { getMainDefinition } from 'apollo-utilities';
 import { ApolloProvider } from '@apollo/react-hooks';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -32,17 +35,36 @@ const theme = createMuiTheme({
     },
   },
 });
+//Apollo Code
+const httpLink = new HttpLink({
+  uri: 'https://react.eogresources.com/graphql'
+});
 
 const wsLink = new WebSocketLink({
-  uri: `ws://localhost:5000/`,
+  uri: `ws://react.eogresources.com/graphql`,
   options: {
     reconnect: true
   }
 });
 
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
 const client = new ApolloClient({
-  uri: `https://react.eogresources.com/graphql`
-})
+  link,
+  cache: new InMemoryCache()
+});
+
 
 client.query({
   query: gql`
