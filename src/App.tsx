fb6080dd0a6f69f17de100802632e37353d2f0ca
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-01-16 06:04:40 -0600

    Live render chart in progress

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,9 @@ import Wrapper from './components/Wrapper';
 import NowWhat from './components/NowWhat';
 import ChartCard from './components/ChartCard';
 import ChartMenu from './components/ChartMenu';
-import Chart from 'react-apexcharts';
-import { WebSocketLink } from 'apollo-link-ws';
-import { split } from 'apollo-link';
-import { HttpLink } from 'apollo-link-http';
-import ApolloClient from 'apollo-client';
-import {InMemoryCache} from 'apollo-boost';
 import { gql } from 'apollo-boost';
-import { getMainDefinition } from 'apollo-utilities';
-import { ApolloProvider } from '@apollo/react-hooks';
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid } from 'recharts';
+import {Subscription} from '@apollo/react-components';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const store = createStore();
@@ -35,62 +29,21 @@ const theme = createMuiTheme({
     },
   },
 });
-//Apollo Code
-const httpLink = new HttpLink({
-  uri: 'https://react.eogresources.com/graphql'
-});
-
-const wsLink = new WebSocketLink({
-  uri: `wss://react.eogresources.com/graphql`,
-  options: {
-    reconnect: true
-  }
-});
-
-const link = split(
-  // split based on operation type
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
-  },
-  wsLink,
-  httpLink
-);
-
-const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache()
-});
 
 const GET_MEASUREMENTS = gql`subscription{newMeasurement{
   metric,
   value,
-  at
+  at,
+  unit
 }
 }`;
 
+let chartData = [] as any;
 
-client.subscribe({
-  query: GET_MEASUREMENTS
-})
-.subscribe({next(data){console.log(data)},
-error(err){console.log(err)}})
-// .catch(err => console.log(err));
-
-class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-
-  render() {
+class App extends Component {
+  
+render() {
     return (
-    <ApolloProvider client={client}>
     <Router>
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
@@ -105,7 +58,38 @@ class App extends Component{
             <Route path='/charts' exact>
               <ChartMenu />
               <ChartCard>
-                {/* <Chart type='line' width='400' /> */}
+                <Subscription subscription={GET_MEASUREMENTS}>
+                  {({data, error, loading}) => {
+                    if (loading) {
+                      return (<h1>Loading</h1>);
+                    }
+
+                    if (error) {console.log('We have an issue sir:', error)}
+
+                    if (data.newMeasurement.metric === 'waterTemp') {
+                      const dataLog = {
+                        x: data.newMeasurement.at,
+                        y: data.newMeasurement.value
+                      };
+                      if (chartData.length > 100) {
+                        chartData.shift()
+                      }
+                      chartData.push(dataLog);
+                      // console.log(chartData.length)
+                    };
+
+                    return (
+                      <div>
+                      <LineChart width={1000} height={500} data={chartData}>
+                        <Line type="monotone" dataKey="y" stroke="#8884d8" />
+                        <XAxis  />
+                        <YAxis />
+                        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                        <Tooltip />
+                      </LineChart>
+                      </div>
+                    )}}
+                </Subscription>
               </ChartCard>
             </Route>
           </Switch>
@@ -114,7 +98,6 @@ class App extends Component{
       </Provider>
     </MuiThemeProvider>
     </Router>
-    </ApolloProvider>
     )
   }
 }
