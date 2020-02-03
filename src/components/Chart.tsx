9fa4f60e53158ccffd32ebb2818364c9d9c511b1
CaptainHdz <47486440+CaptainHdz@users.noreply.github.com>
2020-02-03 12:26:53 -0600

    Multiple Y axis renders

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -7,24 +7,48 @@ export default (props) => {
         <div>
             <LineChart width={1000} height={550}>
                 <XAxis domain={['auto', 1000]} />
-                <YAxis domain={['auto', 1000]} />
+                {/* <YAxis domain={['auto', 1000]} /> */}
                 <Legend />
                 <Tooltip />
+
+                {props.IVOShow &&
+                <YAxis yAxisId='IVO' domain={['auto', 'auto']} />
+                }
                 {props.IVOShow && 
-                <Line dot={false} data={props.IVOData}  type="monotone" dataKey="injValveOpen" stroke="#E7E648" isAnimationActive={false} />
+                <Line yAxisId='IVO' dot={false} data={props.IVOData}  type="monotone" dataKey="injValveOpen" stroke="#E7E648" isAnimationActive={false} />
+                }
+
+                {props.CPShow &&
+                <YAxis yAxisId='CP' domain={['auto', 'auto']} />
                 }
                 {props.CPShow && 
-                <Line dot={false} data={props.CPData} type="monotone"  dataKey="casingPressure" stroke="#82ca9d" isAnimationActive={false} />
+                <Line yAxisId='CP' dot={false} data={props.CPData} type="monotone"  dataKey="casingPressure" stroke="#82ca9d" isAnimationActive={false} />
+                }
+
+                {props.TPShow &&
+                <YAxis yAxisId='TP' domain={['auto', 'auto']} />
                 }
                 {props.TPShow && 
-                <Line dot={false} data={props.TPData} type="monotone" dataKey="tubingPressure" stroke="#f2a353" isAnimationActive={false} />
+                <Line yAxisId='TP' dot={false} data={props.TPData} type="monotone" dataKey="tubingPressure" stroke="#f2a353" isAnimationActive={false} />
+                }
+
+                {props.WTShow &&
+                <YAxis yAxisId='WT' domain={['auto', 'auto']} />
                 }
                 {props.WTShow && 
-                <Line dot={false} data={props.waterData} type="monotone" dataKey="waterTemp" stroke="#7ce348" isAnimationActive={false} />      }
+                <Line yAxisId='WT' dot={false} data={props.waterData} type="monotone" dataKey="waterTemp" stroke="#7ce348" isAnimationActive={false} />      }
+
+                {props.FTShow &&
+                <YAxis yAxisId='FT' domain={['auto', 'auto']} />
+                }
                 {props.FTShow && 
-                <Line dot={false} data={props.flareData} type="monotone" dataKey="flareTemp" stroke="#f42b3f" isAnimationActive={false} />      }
+                <Line yAxisId='FT' dot={false} data={props.flareData} type="monotone" dataKey="flareTemp" stroke="#f42b3f" isAnimationActive={false} />      }
+
+                {props.OTShow &&
+                <YAxis yAxisId='OT' domain={['auto', 'auto']} />
+                }
                 {props.OTShow && 
-                <Line dot={false} data={props.oilData} type="monotone" dataKey="oilTemp" stroke="#311eff" isAnimationActive={false} /> 
+                <Line yAxisId='OT' dot={false} data={props.oilData} type="monotone" dataKey="oilTemp" stroke="#311eff" isAnimationActive={false} /> 
                 }
             </LineChart>
         </div>
